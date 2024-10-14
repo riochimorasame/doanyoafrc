f5189415a9d6c8363abc7e4f59f3222680a2f129
riochimorasame <enochmontcho2@gmail.com>
2024-10-14 08:04:59 +0000

    Add files via upload

---
+++ scripte.js
@@ -0,0 +1,23 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    var nom = document.getElementById('nom').value;
+    var prenom = document.getElementById('prenom').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var pays = document.getElementById('pays').value;
+    var platform = document.getElementById('platform').value;
+    var message = document.getElementById('message').value;
+
+    var mailtoLink = 'mailto:doanyoafrica@gmail.com'
+                    + '?subject=Demande de contact'
+                    + '&body=Nom: ' + nom
+                    + '%0APrénom: ' + prenom
+                    + '%0AEmail: ' + email
+                    + '%0ATéléphone: ' + phone
+                    + '%0APays: ' + pays
+                    + '%0APlateforme de réponse: ' + platform
+                    + '%0A%0AMessage: ' + message;
+    
+    window.location.href = mailtoLink;
+});
